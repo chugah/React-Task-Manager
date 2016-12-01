5961667365e66ea88ee6ac97621180d1581b385b
Sami Barghshoon <chugah@users.noreply.github.com>
2016-12-01 17:44:40 -0500

    Added dictionary

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -12,8 +12,7 @@ class TodoList extends Component {
 						<div className="row">
 							<div className="large-12 large-centered column">
 								<div className="container" id="background-todo-list">
-									<h3 className="container-message">Congratulations!</h3>
-									<p className="container-message">You have finished everything on your list.</p>
+									<h4 className="container-message">AMEE PUDUM KA HEP TO?</h4>
 									<img src= "http://i.imgur.com/zt6Na6V.gif" id="plug-minion" width="100%" height="50%" />
 								</div>				
 							</div>
