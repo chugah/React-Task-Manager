af71aa3259deb25b021d7f9ffcb44b0d07047ddf
Sami Barghshoon <chugah@users.noreply.github.com>
2016-12-06 11:47:08 -0500

    Added css and text to translator component

--- app/components/Translator.jsx
+++ app/components/Translator.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+var minion_button_path = 'images/minion-button.jpg';
+
 class Translator extends Component {
   	componentWillMount () {
     	const script_one = document.createElement("script");
@@ -20,21 +22,28 @@ class Translator extends Component {
 	render(){
 		return (
 			<div>
+				<div className="row">
+					<div className="large-12 large-centered column">
+						<p className="todoapp-copy">It is so much fun to learn Minionese.</p>
+					</div>
+				</div>	
 				<div className="container-translator">
 			        <div className="row">
 			            <div className="large-4 column">
 			            	<h3>English</h3>
-			                <textarea type="text" className="nixfocus mt-patient" id="mt-input" placeholder="Try it out!" />
+			                <textarea type="text" className="nixfocus mt-patient" id="mt-input" placeholder="Enter a word or sentence..." />
 			            </div>
 			            <hr />
 			            <div className="large-4 column">
 			            	<h3 align="center">Magic Button</h3>
-			                <button id="mt-button" className="expanded hollow button">CLICK ME</button>
+			                <button id="mt-button" className="expanded hollow button">
+			                <img id="minion-button" src={minion_button_path} />
+			                </button>
 			            </div>
 			            <hr />
 			            <div className="large-4 column">
 			                <h3 align="right">Minionese</h3>
-			                <textarea type="text" className="nixfocus mt-patient" id="mt-output" placeholder="Click 'Translate it'!" />
+			                <textarea type="text" className="nixfocus mt-patient" id="mt-output" placeholder="..and click the Magic Button!" />
 			            </div>
 			        </div>
 				</div>
